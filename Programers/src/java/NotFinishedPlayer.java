319f8b41b669a44b743061c96039be104b50ab78
Beom-Chu <chorocsky@gmail.com>
2021-03-29 16:29:26 +0900

    add

---
+++ Programers/src/java/NotFinishedPlayer.java
@@ -0,0 +1,25 @@
+package java;
+
+//완주하지 못한 선수
+
+public class NotFinishedPlayer {
+
+	public static String solution(String[] participant, String[] completion) {
+        String answer = "a";
+        
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		
+		String[] participant = {};
+		String[] completion = {};
+		
+		
+		String rtn = solution(participant, completion);
+		
+		System.out.println(rtn);
+		
+	}
+
+}
