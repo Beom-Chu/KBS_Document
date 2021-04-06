74f0bc662b36ecab4e6c9b31d3cbf7261fddfabb
Beom-Chu <chorocsky@gmail.com>
2021-04-06 17:01:13 +0900

    add - setProperties

--- coding/src/property/PropertiesDemo.java
+++ coding/src/property/PropertiesDemo.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
 
 public class PropertiesDemo {
@@ -43,6 +46,12 @@ public class PropertiesDemo {
 		properties.store(stream, "프로퍼티 파일");
 		stream.close();
 	}
+	
+	private void setProperties(List<HashMap<String,String>> listMap) {
+		for(HashMap<String,String> map : listMap) {
+			properties.setProperty(map.get("key"),map.get("value"));
+		}
+	}
 
 	public static void main(String[] args) throws Exception {
 		
@@ -65,6 +74,10 @@ public class PropertiesDemo {
 		for (int i = 0; i < 10; i++) {
 			properties.setProperty("KEY" + i, "VALUE" + Math.round(Math.random()*10));
 		}
+		
+		// 프로퍼티 ListMap 설정
+//		List<HashMap<String,String>> listMap = new ArrayList<HashMap<String,String>>();
+//		propertiesDemo.setProperties(listMap);
 	
 		
 		properties.list(System.out); // 프로퍼티 값 출력
