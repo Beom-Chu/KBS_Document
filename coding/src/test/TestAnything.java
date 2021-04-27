de34f674a4a96a24efff9f51b097d6aa188f2472
Beom-Chu <chorocsky@gmail.com>
2021-04-27 18:34:14 +0900

    add

--- coding/src/test/TestAnything.java
+++ coding/src/test/TestAnything.java
@@ -10,9 +10,11 @@ class TestAnything {
 		try (
 				Scanner sc = new Scanner(System.in);
 				){
+			
 			int A = sc.nextInt();
 			int B = sc.nextInt();
 			int C = sc.nextInt();
+			
 			System.out.println((A+B)%C);
 			System.out.println(((A%C) + (B%C))%C);
 			System.out.println((A*B)%C);
