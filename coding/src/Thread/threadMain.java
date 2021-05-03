6d57858c570ef6bb11c6815bd27425130aab6cc3
Beom-Chu <chorocsky@gmail.com>
2021-05-03 10:11:25 +0900

    try with resource

--- coding/src/Thread/threadMain.java
+++ coding/src/Thread/threadMain.java
@@ -11,37 +11,38 @@ public class threadMain {
 		
 		System.out.print("Thread:1\nRunable:2\n입력:");
 		
-		InputStream is = System.in;
-		InputStreamReader isr = new InputStreamReader(is);
-		BufferedReader br = new BufferedReader(isr);
-		
-		String type = null;
-		try {
-			type = br.readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
+		try(
+			InputStream is = System.in;
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr)
+		){
+			String type = null;
+			try {
+				type = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			if(type.equals("1")) {
+				
+				PrintThread prt1 = new PrintThread("A");
+				PrintThread prt2 = new PrintThread("B");
+				
+				prt1.start();
+				prt2.start();
+				
+			}else if(type.equals("2")) {
+				
+				PrintRunable prt1 = new PrintRunable("A");
+				PrintRunable prt2 = new PrintRunable("B");
+				
+				Thread trd1 = new Thread(prt1);
+				Thread trd2 = new Thread(prt2);
+				
+				trd1.start();
+				trd2.start();
+			}
 		}
-		
-		if(type.equals("1")) {
-			
-			PrintThread prt1 = new PrintThread("A");
-			PrintThread prt2 = new PrintThread("B");
-			
-			prt1.start();
-			prt2.start();
-			
-		}else if(type.equals("2")) {
-			
-			PrintRunable prt1 = new PrintRunable("A");
-			PrintRunable prt2 = new PrintRunable("B");
-			
-			Thread trd1 = new Thread(prt1);
-			Thread trd2 = new Thread(prt2);
-			
-			trd1.start();
-			trd2.start();
-		}
-		
 		
 		
 		
